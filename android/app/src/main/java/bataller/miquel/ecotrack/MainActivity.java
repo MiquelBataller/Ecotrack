5fb77108e518eea0ec7c7f8422ac68e858577981
MiquelBataller <miquelbdc06@gmail.com>
2026-05-15 20:36:05 +0200

    APK

---
+++ android/app/src/main/java/bataller/miquel/ecotrack/MainActivity.java
@@ -0,0 +1,5 @@
+package bataller.miquel.ecotrack;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bataller.miquel.ecotrack',
+  appName: 'EcoTrack',
+  webDir: 'dist/Ecotrack/browser'
+};
+
+export default config;
